edc92f6bf3782c433c73314f8376f969c38d2fb8
Pierre Gueguen <gueguenpierre.pro@gmail.com>
2026-06-18 17:36:32 +0200

    corel version

--- TP_ADC_RasPico/files/main.py
+++ TP_ADC_RasPico/files/main.py
@@ -1,48 +1,54 @@
 from machine import Timer , Pin , ADC
 
-SAMPLE_RATE_HZ = 500
+SAMPLE_RATE_HZ = 100
 SAMPLE_PERIOD_MS = 1000 // SAMPLE_RATE_HZ
-WINDOW_PERIOD_MS = 2048
-WINDOW_SAMPLE_COUNT = WINDOW_PERIOD_MS // SAMPLE_PERIOD_MS
-BPM_TO_HZ = 1 / 60
+BUFFER_PERIOD_MS = 4000
+BUFFER_SAMPLE_COUNT = BUFFER_PERIOD_MS // SAMPLE_PERIOD_MS
 led = Pin(25, Pin.OUT)
 adc = ADC(Pin(26 , mode=Pin.IN))
 
-raw_buffer = [0] * WINDOW_SAMPLE_COUNT
+raw_buffer = [0] * BUFFER_SAMPLE_COUNT
+
 
 def adc_handler(_):
     raw_buffer.append(adc.read_u16())
     raw_buffer.pop(0)
     #print(adc.read_u16())
 
+def pierrecorrelation(buffer: list[float]) -> list[float]:
+    output = []
+
+    for offset in range(0, len(buffer)):
+        total_diff = 0
+        for i in range(offset, len(buffer)):
+            total_diff += abs(buffer[i] - buffer[i - offset])
+            # total_diff += (buffer[i] - buffer[i - offset]) ** 2
+        total_diff /= len(buffer) - offset
+        output.append(total_diff)
+
+    return output
+
+
+def first_minimum_index(ys: list[float]) -> int:
+    for i in range(1, len(ys) - 1):
+        if ys[i - 1] > ys[i] and ys[i] < ys[i + 1]:
+            return i
+    return -1
+
+
+def offset_to_bpm(offset: int, sample_rate: float) -> float:
+    if offset <= 0:
+        return 0.0
+    period = offset / sample_rate  # in seconds
+    bpm = 60.0 / period
+    return bpm
+
 def compute_handler(_):
-
-    min_sample = min(raw_buffer)
-    max_sample = max(raw_buffer)
-    threshold = (min_sample + max_sample) // 2
-
-    rising_edges = []
-
-    for i in range(WINDOW_SAMPLE_COUNT - 1):
-        if raw_buffer[i] < threshold and raw_buffer[i + 1] >= threshold:
-            rising_edges.append(i)
-    
-    nb_rising_edges = len(rising_edges)
-    if nb_rising_edges < 2:
-        return
-
-    current_rising_edge_index = 0
-    next_rising_edge_index = 1
-
-    while next_rising_edge_index < nb_rising_edges:
-        period_samples = rising_edges[next_rising_edge_index] - rising_edges[current_rising_edge_index]
-        bpm = 30_000 // period_samples
-        if bpm >= 50 and bpm < 200:
-            print("BPM:", bpm)
-            current_rising_edge_index = next_rising_edge_index
-            next_rising_edge_index = current_rising_edge_index + 1
-        else:
-            next_rising_edge_index += 1
+    frozen_buffer = raw_buffer.copy()
+    corr = pierrecorrelation(frozen_buffer)
+    min_offest = first_minimum_index(corr)
+    bpm = offset_to_bpm(min_offest, sample_rate=SAMPLE_RATE_HZ)
+    print("BPM: ", bpm)
 
 if __name__ == "__main__":
     adc_timer = Timer(
@@ -51,8 +57,5 @@ if __name__ == "__main__":
         callback=adc_handler
     )
 
-    compute_timer = Timer(
-        mode=Timer.PERIODIC,
-        period=WINDOW_PERIOD_MS//8,
-        callback=compute_handler
-    )
+    while True:
+        compute_handler(None)
